22d56a2ca2388872f428b308ba4ac23dea13aa64
Samuel Pfrommer <sam.pfrommer@gmail.com>
2019-05-07 22:19:10 -0400

    Add forwards direction sliding sim

--- dynamics.py
+++ dynamics.py
@@ -1,17 +1,32 @@
+import sys
 import math
 import numpy as np
-import lemkelcp as lcp
 from collections import namedtuple
 
-SimParams = namedtuple('params', 'g x0 xdot0 lambda0 dt time_steps')
+import lemkelcp as lcp
+
+# x corresponds to height
+FallingSimParams = namedtuple('falling_params', 'g x0 xdot0 lambda0 dt time_steps')
+# x corresponds to sliding velocity, u external force
+SlidingSimParams = namedtuple('sliding_params', 'g x0 xdot0 mu dt u time_steps')
 
 def main():
-    p = SimParams(x0=20.0,  xdot0=4.0,  lambda0=0.0,
-                  g=-1.0,    dt=1.0,     time_steps=20)
-    xs, xdots, lambdas = falling_box_sim(p)
-    print(np.hstack((xs, xdots, lambdas)))
+    if sys.argv[1] == "falling":
+        print('Solving falling sim...')
+        p = FallingSimParams(x0=20.0,  xdot0=4.0,  lambda0=0.0,
+                      g=1.0,    dt=1.0,     time_steps=30)
+        xs, xdots, lambdas = falling_box_sim(p)
+        print(np.hstack((xs, xdots, lambdas)))
 
-# Takes in parameters as a named tuple
+    if sys.argv[1] == "sliding":
+        print('Solving sliding sim...')
+        p = SlidingSimParams(x0=0.0,  xdot0=6.0,  mu=1.0,
+                      g=1.0,   dt=1.0,     time_steps=30,
+                      u = np.ones(30))
+        xs, xdots, lambdas = sliding_box_sim(p)
+        print(np.hstack((xs, xdots, lambdas)))
+
+# Takes in parameters as a FallingSimParams
 def falling_box_sim(p):
     xs = np.zeros((p.time_steps, 1))
     xs[0] = p.x0
@@ -23,16 +38,48 @@ def falling_box_sim(p):
     for t in range(p.time_steps-1):
         M = np.array([[(1/p.dt)**2]])
         q = np.array([-(1/(p.dt**2)) *
-            (xs[t] + xdots[t] * p.dt + p.g * (p.dt**2))])
+            (xs[t] + xdots[t] * p.dt - p.g * (p.dt**2))])
         xsol, exit_code, _ = lcp.lemkelcp(M,q)
         assert(exit_code == 0)
         
         xs[t+1] = xsol
-        if np.isclose(xs[t+1], 0):
-            lambdas[t+1] = M[0,0] * xs[t+1] + q[0]
+
+        # Solver doesn't output slack variable
+        # So we calculate it explicitely
+        if np.isclose(xsol, 0):
+            lambdas[t+1] = M[0,0] * xsol + q[0]
         else:
             lambdas[t+1] = 0
-        xdots[t+1] = xdots[t] + (p.g + lambdas[t+1]) * p.dt
+        xdots[t+1] = xdots[t] + (-p.g + lambdas[t+1]) * p.dt
+
+    return xs, xdots, lambdas
+
+# Takes in parameters as a SlidingSimParams
+def sliding_box_sim(p):
+    xs = np.zeros((p.time_steps, 1))
+    xs[0] = p.x0
+    xdots = np.zeros((p.time_steps, 1))
+    xdots[0] = p.xdot0
+    # Lambda corresponds to unused friction force, see notes
+    # First time step all friction force is unused
+    lambdas = np.zeros((p.time_steps, 1))
+    lambdas[0] = p.mu * p.g
+
+    for t in range(p.time_steps-1):
+        M = np.array([[1/p.dt]])
+        q = np.array([-(1/p.dt) * xdots[t] - p.u[t+1] + p.mu * p.g])
+        xdotsol, exit_code, _ = lcp.lemkelcp(M,q)
+        assert(exit_code == 0)
+        
+        xdots[t+1] = xdotsol
+        xs[t+1] = xs[t] + p.dt * xdots[t+1]
+
+        # Solver doesn't output slack variable
+        # So we calculate it explicitely
+        if np.isclose(xdotsol, 0):
+            lambdas[t+1] = M[0,0] * xdotsol + q[0]
+        else:
+            lambdas[t+1] = 0
 
     return xs, xdots, lambdas
 
--- gen.py
+++ gen.py
@@ -7,16 +7,16 @@ from dynamics import SimParams
 
 import progressbar
 
-g = -1.0
+g = 1.0
 dt = 1.0
 time_steps = 30
 
 runs = 1000
 
-def random_params():
-    p = SimParams(x0=rand.uniform(0,20),   xdot0=rand.uniform(-5,5),
-                  lambda0=0.0,              g=g,
-                  dt=dt,                    time_steps=time_steps)
+def random_falling_params():
+    p = FallingSimParams(x0=rand.uniform(0,20),   xdot0=rand.uniform(-5,5),
+                         lambda0=0.0,             g=g,
+                         dt=dt,                   time_steps=time_steps)
     return p
 
 # Lines up xs/xdots from one time stop to lambdas of the next
@@ -32,7 +32,6 @@ def format_data(xs, xdots, lambdas):
 def main():
     run_rows = time_steps - 1
     dataset = np.zeros((runs * run_rows, 3))
-
     
     print('Generating {} runs...'.format(runs))
     bar = progressbar.ProgressBar(maxval=runs, \
@@ -41,7 +40,7 @@ def main():
 
     for i in range(runs):
         bar.update(i+1)
-        p = random_params()
+        p = random_falling_params()
         xs, xdots, lambdas = dynamics.falling_box_sim(p)
         data = format_data(xs, xdots, lambdas)
         dataset[i * run_rows : (i+1) * run_rows, :] = data
