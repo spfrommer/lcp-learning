3270a75b39118e90f52d040a0441eaf0b89773b9
Samuel Pfrommer <sam.pfrommer@gmail.com>
2019-05-08 00:13:56 -0400

    Broken version of sliding box dynamics with slack vars

--- dynamics.py
+++ dynamics.py
@@ -2,6 +2,7 @@ import sys
 import math
 import numpy as np
 from collections import namedtuple
+import pdb
 
 import lemkelcp as lcp
 
@@ -16,15 +17,21 @@ def main():
         p = FallingSimParams(x0=20.0,  xdot0=4.0,  lambda0=0.0,
                       g=1.0,    dt=1.0,     time_steps=30)
         xs, xdots, lambdas = falling_box_sim(p)
+        print('x, xdot, lambda')
         print(np.hstack((xs, xdots, lambdas)))
 
     if sys.argv[1] == "sliding":
         print('Solving sliding sim...')
-        p = SlidingSimParams(x0=0.0,  xdot0=6.0,  mu=1.0,
+        p = SlidingSimParams(x0=0.0,  xdot0=1.0,  mu=0.0,
                       g=1.0,   dt=1.0,     time_steps=30,
-                      u = np.ones(30))
-        xs, xdots, lambdas = sliding_box_sim(p)
-        print(np.hstack((xs, xdots, lambdas)))
+                      u = np.ones(30) * 0)
+        xs, posxdots, negxdots, poslambdas, neglambdas = sliding_box_sim(p)
+        print('Full output: ')
+        print('x, xdot+, xdot-, lambda+, lambda-')
+        print(np.hstack((xs, posxdots, negxdots, poslambdas, neglambdas)))
+        print('Summed output: ')
+        print('x, xdot, lambda')
+        print(np.hstack((xs, posxdots - negxdots, poslambdas - neglambdas)))
 
 # Takes in parameters as a FallingSimParams
 def falling_box_sim(p):
@@ -39,17 +46,16 @@ def falling_box_sim(p):
         M = np.array([[(1/p.dt)**2]])
         q = np.array([-(1/(p.dt**2)) *
             (xs[t] + xdots[t] * p.dt - p.g * (p.dt**2))])
-        xsol, exit_code, _ = lcp.lemkelcp(M,q)
+        xsol, exit_code, _ = lcp.lemkelcp(M, q)
         assert(exit_code == 0)
         
         xs[t+1] = xsol
 
         # Solver doesn't output slack variable
-        # So we calculate it explicitely
+        # So we calculate it explicitely (otherwise just zero)
         if np.isclose(xsol, 0):
             lambdas[t+1] = M[0,0] * xsol + q[0]
-        else:
-            lambdas[t+1] = 0
+            
         xdots[t+1] = xdots[t] + (-p.g + lambdas[t+1]) * p.dt
 
     return xs, xdots, lambdas
@@ -58,29 +64,35 @@ def falling_box_sim(p):
 def sliding_box_sim(p):
     xs = np.zeros((p.time_steps, 1))
     xs[0] = p.x0
-    xdots = np.zeros((p.time_steps, 1))
-    xdots[0] = p.xdot0
+    posxdots = np.zeros((p.time_steps, 1))
+    posxdots[0] = p.xdot0 * (np.sign(p.xdot0) > 0)
+    negxdots = np.zeros((p.time_steps, 1))
+    negxdots[0] = (-p.xdot0) * (np.sign(p.xdot0) < 0)
     # Lambda corresponds to unused friction force, see notes
     # First time step all friction force is unused
-    lambdas = np.zeros((p.time_steps, 1))
-    lambdas[0] = p.mu * p.g
+    poslambdas = np.zeros((p.time_steps, 1))
+    poslambdas[0] = p.mu * p.g
+    neglambdas = np.zeros((p.time_steps, 1))
+    neglambdas[0] = p.mu * p.g
 
     for t in range(p.time_steps-1):
-        M = np.array([[1/p.dt]])
-        q = np.array([-(1/p.dt) * xdots[t] - p.u[t+1] + p.mu * p.g])
-        xdotsol, exit_code, _ = lcp.lemkelcp(M,q)
+        M = (1/p.dt) * np.array([[0,1],[1,0]])
+        q = np.array([(1/p.dt) * posxdots[t] - (1/p.dt) * negxdots[t] + p.u[t+1] + p.mu * p.g,
+                      (1/p.dt) * negxdots[t] - (1/p.dt) * posxdots[t] - p.u[t+1] + p.mu * p.g])
+        xdotsol, exit_code, _ = lcp.lemkelcp(M, q)
         assert(exit_code == 0)
         
-        xdots[t+1] = xdotsol
-        xs[t+1] = xs[t] + p.dt * xdots[t+1]
+        posxdots[t+1] = xdotsol[0]
+        negxdots[t+1] = xdotsol[1]
+        xs[t+1] = xs[t] + p.dt * (posxdots[t+1] - negxdots[t+1])
 
         # Solver doesn't output slack variable
         # So we calculate it explicitely
-        if np.isclose(xdotsol, 0):
-            lambdas[t+1] = M[0,0] * xdotsol + q[0]
-        else:
-            lambdas[t+1] = 0
+        if np.isclose(xdotsol[0], 0):
+            poslambdas[t+1] = np.dot(M[0,:], xdotsol) + q[0]
+        if np.isclose(xdotsol[1], 0):
+            neglambdas[t+1] = np.dot(M[1,:], xdotsol) + q[1]
 
-    return xs, xdots, lambdas
+    return xs, posxdots, negxdots, poslambdas, neglambdas
 
 if __name__ == "__main__": main()
