59d5d8b99ee4a4283ab782c280990f85ab39e6f9
Samuel Pfrommer <sam.pfrommer@gmail.com>
2019-05-07 22:17:58 -0400

    Add new training and plotting options

--- plot.py
+++ plot.py
@@ -3,21 +3,46 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-mpl.rcParams['figure.dpi'] = 300
+from matplotlib import cm
 
-data = np.load('out/data.npy')
+import torch
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+def main():
+    plot_data('out/data.npy')
 
-point_count = 3000
-#ax.scatter(data[0:29,0], data[0:29,1], data[0:29,2])
-ax.scatter(data[:point_count, 0],
-           data[:point_count, 1],
-           data[:point_count, 2])
+def plot_data(ax, path):
+    data = np.load(path)
 
-ax.set_xlabel('x')
-ax.set_ylabel('xdot')
-ax.set_zlabel('lambda')
 
-plt.show()
+    point_count = 3000
+    ax.scatter(data[:point_count, 0],
+               data[:point_count, 1],
+               data[:point_count, 2])
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('xdot')
+    ax.set_zlabel('lambda')
+
+def plot_net(ax, net, xrange, xdotrange):
+    xs = torch.tensor(np.linspace(xrange[0], xrange[1], 30))
+    xdots = torch.tensor(np.linspace(xdotrange[0], xdotrange[1], 30))
+
+    grid_x, grid_xdot = torch.meshgrid(xs, xdots)
+    xs_vec = grid_x.reshape(-1, 1).float()
+    xdots_vec = grid_xdot.reshape(-1, 1).float()
+
+    lambdas = net(torch.cat((xs_vec, xdots_vec), dim=1))
+    grid_lambdas = lambdas.reshape(grid_x.shape)
+
+    ax.plot_surface(grid_x.detach().numpy(),
+                    grid_xdot.detach().numpy(),
+                    grid_lambdas.detach().numpy(),
+                    cmap = cm.hot, alpha=0.2)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('xdot')
+    ax.set_zlabel('lambda')
+
+    plt.show()
+
+if __name__ == "__main__": main()
--- train.py
+++ train.py
@@ -1,52 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.utils.data as Data
 
-import numpy as np
+import plot
+epochs = 200
 
+def main():
+    states, lambdas = load_data('out/data.npy')
 
-data = np.load('out/data.npy')
-x = torch.from_numpy(data[:, 0:1])
-y = torch.from_numpy(data[:, 2])
+    net = Net(n_feature=states.size(1), n_hidden=3, n_output=lambdas.size(1))
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.2)
+    loss_func = torch.nn.MSELoss()
 
-# torch can only train on Variable, so convert them to Variable
-x, y = Variable(x), Variable(y)
+    for t in range(epochs):
+        lambdas_pred = net(states)
+        loss = loss_func(lambdas_pred, lambdas)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        loss_val = loss.data.numpy()
 
-# this is one way to define a network
+    print('Finished training with loss: {}'.format(loss_val.item()))
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    plot.plot_data(ax, 'out/data.npy')
+    plot.plot_net(ax, net, [0, 30], [-10, 10])
+    plt.show()
+
+def load_data(path):
+    data = np.load(path)
+    states = torch.from_numpy(data[:, 0:2])
+    lambdas = torch.from_numpy(data[:, 2])
+
+    states, lambdas = Variable(states), Variable(lambdas)
+    lambdas = lambdas.view(-1, 1)
+    return states, lambdas
+
 class Net(torch.nn.Module):
     def __init__(self, n_feature, n_hidden, n_output):
         super(Net, self).__init__()
-        self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
-        self.predict = torch.nn.Linear(n_hidden, n_output)   # output layer
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)
+        self.predict = torch.nn.Linear(n_hidden, n_output)
 
     def forward(self, x):
-        x = F.relu(self.hidden(x))      # activation function for hidden layer
-        x = self.predict(x)             # linear output
+        x = F.relu(self.hidden(x))
+        x = self.predict(x)
         return x
 
-epochs = 200
-
-net = Net(n_feature=1, n_hidden=10, n_output=1)     # define the network
-# print(net)  # net architecture
-optimizer = torch.optim.SGD(net.parameters(), lr=0.2)
-loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
-
-# train the network
-for t in range(epochs):
-    print('Training epoch: {}'.format(t))
-    print('Predicting')
-    prediction = net(x)     # input x and predict based on x
-
-    print('Calculating loss')
-    loss = loss_func(prediction, y)     # must be (1. nn output, 2. target)
-
-    print('Backpropping')
-    optimizer.zero_grad()   # clear gradients for next train
-    loss.backward()         # backpropagation, compute gradients
-
-    print('Applying gradients')
-    optimizer.step()        # apply gradients
-    
-    loss_val = loss.data.numpy()
-    print(loss_val)
+if __name__ == "__main__": main()
