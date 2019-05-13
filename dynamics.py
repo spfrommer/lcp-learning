Fix force calculation

dynamics.py
@@ -40,9 +40,9 @@ def main():
 
     if opts.simtype == SimType.SLIDING:
         print('Solving sliding sim...')
-        p = SlidingSimParams(x0=0.0,  xdot0=1,  mu=0.0,
+        p = SlidingSimParams(x0=0.0,  xdot0=-4,  mu=2.0,
                       g=1.0,   dt=1.0,     time_steps=30,
-                      us = (0) * np.ones(30))
+                      us = (1) * np.ones(30))
         sol = sliding_box_sim(p)
         print('Full output: ')
         print('x, xdot+, xdot-, lambda\'+, lambda\'-')
@@ -89,12 +89,10 @@ def sliding_box_sim(p):
     posxdots[0] = p.xdot0 * (np.sign(p.xdot0) > 0)
     negxdots = np.zeros((p.time_steps, 1))
     negxdots[0] = (-p.xdot0) * (np.sign(p.xdot0) < 0)
-    # Lambda corresponds to unused friction force, see notes
-    # First time step all friction force is unused
+    # Lambda corresponds roughly to unused friction force, see notes
+    # Just initialize to zero, depends on previous dynamics vars
     poslambdas = np.zeros((p.time_steps, 1))
-    poslambdas[0] = p.mu * p.g
     neglambdas = np.zeros((p.time_steps, 1))
-    neglambdas[0] = p.mu * p.g
 
     for t in range(p.time_steps-1):
         M = (1/p.dt) * np.eye(2)
@@ -110,10 +108,11 @@ def sliding_box_sim(p):
         # Solver doesn't output slack variable
         # So we calculate it explicitely
         if np.isclose(xdotsol[0], 0):
-            poslambdas[t+1] = np.dot(M[1,:], xdotsol) + q[1]
-        if np.isclose(xdotsol[1], 0):
+            # Pos velocity and negative friction are complementary
             neglambdas[t+1] = np.dot(M[0,:], xdotsol) + q[0]
-            pdb.set_trace()
+        if np.isclose(xdotsol[1], 0):
+            # Negative velocity and positive friction are complementary
+            poslambdas[t+1] = np.dot(M[1,:], xdotsol) + q[1]
     
     # Make sure both velocities can't be positive at the same time
     assert((np.isclose(posxdots * negxdots, 0)).all())
@@ -123,24 +122,14 @@ def sliding_box_sim(p):
 def process_sliding_solution(sol, params):
     xdots = sol.posxdots - sol.negxdots;
     # Pos/neg friction forces (solution lambdas are actually slack forces)
-    lambdaforces = params.mu * params.g - np.hstack((sol.poslambdas, sol.neglambdas));
-    lambdas = np.zeros(xdots.shape)
-    for i in range(np.size(xdots)):
-        xdot = xdots[i]
-        
-        if np.isclose(xdot, 0):
-            # If stationary, take the larger static friction force
-            # Other will be negative
-            if lambdaforces[i, 0] > lambdaforces[i, 1]:
-                lambdas[i] = lambdaforces[i, 0]
-            else:
-                lambdas[i] = -lambdaforces[i, 1]
-        else:
-            # If box moving, take friction force in opposite dir
-            # Should be just max friction force
-            dir = np.sign(xdot)
-            lambdas[i] = (-dir) * lambdaforces[i, int(dir == 0)]
-            
+    lambda_forces = params.mu * params.g - np.hstack((sol.poslambdas, sol.neglambdas))
+    # One force will usually be positive, one negative
+    # We want the positive one and the associated sign
+    lambdas = np.expand_dims(np.amax(lambda_forces, axis=1), axis=1)
+    lambda_signs = np.expand_dims(lambda_forces.argmax(axis=1), axis=1)
+    lambda_signs[lambda_signs == 1] = -1
+    lambda_signs[lambda_signs == 0] = 1
+    lambdas = lambdas * lambda_signs
     return SlidingSimSolutionProcessed(sol.xs, xdots, lambdas)
 
 if __name__ == "__main__": main()