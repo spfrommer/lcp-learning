6e1dfac7ed8df96c61e4917572de17730e6613ed
Samuel Pfrommer <sam.pfrommer@gmail.com>
2019-07-24 14:53:31 -0400

    Update presentation

--- presentation/training_graphs.py
+++ presentation/training_graphs.py
@@ -1,8 +1,14 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import pdb
 
 evolutions = np.load('soft_evolution.npy')
+
+font = {'family' : 'normal',
+        'size'   : 30}
+
+matplotlib.rc('font', **font)
 
 for ev in evolutions:
     plt.plot(ev[:, 0], ev[:, 1], color='orange', alpha=0.5, linewidth=4)
@@ -11,4 +17,6 @@ plt.xlim([0,100])
 plt.xlabel('Step #')
 plt.ylabel('Friction coefficient')
 plt.title('Soft constraints')
-plt.show()
+plt.show(block=False)
+
+pdb.set_trace()
