aa656c5e8be2b66f04c7b2e541e424b8b30345d4
Samuel Pfrommer <sam.pfrommer@gmail.com>
2019-08-01 08:57:32 -0400

    Abstract forward learning function

---
+++ qp_experiments/forwards.py
@@ -0,0 +1,146 @@
+import torch
+
+from qpth.qp import QPFunction
+
+import pdb
+
+def get_fowards_function(soft_nonnegativity, soft_lambdas,
+        force_max_dissipation_qp, force_max_dissipation_ext):
+
+    def forward(params, data):
+        mu = params.mu
+
+        n = data.shape[0]
+        vs = torch.unsqueeze(data[:, 0], 1)
+        vnexts = torch.unsqueeze(data[:, 1], 1)
+        us = torch.unsqueeze(data[:, 2], 1)
+
+        beta = vnexts - vs - us
+        
+        pad_base = torch.zeros(6)
+
+        G = torch.tensor([[1.0, -1, 1], [-1, 1, 1], [-1, -1, 0]]).repeat(n, 1, 1)
+        
+        Gpad = torch.tensor([[1.0, -1, 1, 0, 0, 0, 0, 0, 0],
+                             [-1, 1, 1, 0, 0, 0, 0, 0, 0],
+                             [-1, -1, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0]]).repeat(n, 1, 1)
+        
+        fmats = torch.tensor([[1.0, 1, 0], 
+                              [-1, -1, 0],
+                              [0, 0, 1]]).unsqueeze(0).repeat(n, 1, 1)
+        fvecs = torch.cat((vs, us, mu * torch.ones(vs.shape)), 1).unsqueeze(2)
+        f = torch.bmm(fmats, fvecs)
+        batch_zeros = torch.zeros(f.shape)
+        fpad = torch.cat((f, batch_zeros, batch_zeros), 1)
+        
+        # For prediction error
+        A = torch.tensor([[1.0, -1, 0, 0, 0, 0, 0, 0, 0],
+                          [-1, 1, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0]]).repeat(n, 1, 1)
+        beta_zeros = torch.zeros(beta.shape)
+        b = torch.cat((-2 * beta, 2 * beta, beta_zeros, beta_zeros, beta_zeros, 
+                       beta_zeros, beta_zeros, beta_zeros, beta_zeros), 1).unsqueeze(2)
+        
+        inequality_slack_penalty = torch.tensor([0.0, 0, 0, 1, 1, 1, 0, 0, 0]).repeat(n, 1).unsqueeze(2)
+        lambdas_slack_penalty = torch.tensor([0.0, 0, 0, 0, 0, 0, 1, 1, 1]).repeat(n, 1).unsqueeze(2)
+
+        a1 = 1
+        a2 = 1
+        a3 = 50
+        a4 = 50
+        a5 = 0.1
+
+        Q = 2 * a1 * A + 2 * a2 * Gpad
+        p = a1 * b + a2 * fpad + a3 * inequality_slack_penalty + a4 * lambdas_slack_penalty
+        
+        # Constrain slacks (but not lambdas) to be >= 0
+        R = torch.cat((torch.zeros(6, 3), -torch.eye(6)), 1).repeat(n, 1, 1)
+        h = torch.zeros((1, 6)).repeat(n, 1).unsqueeze(2)
+
+        I = torch.eye(3).repeat(n, 1, 1)
+
+        if soft_nonnegativity:
+            # Constrain G lambda + s(1:3) >= 0
+            R = torch.cat((R, -torch.cat((G, I, torch.zeros(n, 3, 3)), 2)), 1)
+        else:
+            # Constrain G lambda + f >= 0
+            R = torch.cat((R, torch.cat((-G, torch.zeros(n, 3, 6)), 2)), 1)
+
+        # This is the same with soft or hard nonnegativity constraint
+        h = torch.cat((h, f), 1)
+        
+        if soft_lambdas:
+            # Constrain lambda + s(4:7) >= 0
+            R = torch.cat((R, -torch.cat((I, torch.zeros(n, 3, 3), I), 2)), 1)
+            h = torch.cat((h, torch.zeros(n, 3, 1)), 1)
+        else:
+            # Constrain lambda >= 0
+            R = torch.cat((R, torch.cat((-I, torch.zeros(n, 3, 6)), 2)), 1)
+            h = torch.cat((h, torch.zeros(n, 3, 1)), 1)
+        
+        if force_max_dissipation_qp:
+            # Factor 2 for 1/2 in front of quadratic term
+            max_dissipation_mat = a5 * 2 * torch.tensor([[1.0, 1, 0, 0, 0, 0, 0, 0, 0],
+                                              [1, 1, 0, 0, 0, 0, 0, 0, 0],
+                                              [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                              [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                              [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                              [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                              [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                              [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                              [0, 0, 0, 0, 0, 0, 0, 0, 0]]).repeat(n, 1, 1) 
+            max_dissipation_vec = a5 * torch.tensor([[-2 * mu, -2 * mu, 0, 0, 0, 0, 0, 0, 0]]).repeat(n, 1).unsqueeze(2) 
+
+            Q = Q + max_dissipation_mat
+            p = p + max_dissipation_vec
+
+
+        Qmod = 0.5 * (Q + Q.transpose(1, 2)) + 0.001 * torch.eye(9).repeat(n, 1, 1)
+        
+        z = QPFunction(check_Q_spd=False)(Qmod, p.squeeze(2), R, h.squeeze(2), torch.tensor([]), torch.tensor([]))
+
+        lcp_slack = torch.bmm(Gpad, z.unsqueeze(2)) + fpad
+
+        costs = 0.5 * torch.bmm(z.unsqueeze(1), torch.bmm(Qmod, z.unsqueeze(2))) \
+                + torch.bmm(p.transpose(1, 2), z.unsqueeze(2)) + a1 * beta**2
+
+        if force_max_dissipation_qp:
+            max_dissipation_const_cost = a5 * mu ** 2
+            max_dissipation_cost = 0.5 * torch.bmm(z.unsqueeze(1), torch.bmm(max_dissipation_mat, z.unsqueeze(2))) \
+                + torch.bmm(max_dissipation_vec.transpose(1, 2), z.unsqueeze(2)) + max_dissipation_const_cost
+            # max dissipation cost is already in QP (just outputted for debugging), need to add const term
+            costs = costs + torch.ones(costs.shape) * max_dissipation_const_cost
+
+        if force_max_dissipation_ext:
+            lambda_plus = z[:, 0]
+            lambda_minus = z[:, 1]
+            mgmu = mu * torch.ones(lambda_plus.shape)
+
+            max_dissipation_cost = a5 * (mgmu - lambda_plus - lambda_minus) ** 2
+            costs = costs + max_dissipation_cost
+
+        outputs = {'cost': sum(costs), 'lambdaPlus': z[0,0],
+                'lambdaMinus': z[0, 1], 'gamma': z[0, 2],
+                'slack[0]': lcp_slack[0,0], 'slack[1]': lcp_slack[0, 1],
+                'slack[2]': lcp_slack[0, 2]}
+        
+        if force_max_dissipation_qp:
+            outputs['max_diss_cost'] = max_dissipation_cost 
+        if force_max_dissipation_ext:
+            outputs['max_diss_cost'] = sum(max_dissipation_cost)
+
+        return torch.sum(costs), outputs
+
+    return forward
--- qp_experiments/physics_test.py
+++ qp_experiments/physics_test.py
@@ -4,17 +4,28 @@ from torch.autograd import Function, Variable
 from torch.nn.parameter import Parameter
 import torch.nn.functional as F
 
-from qpth.qp import QPFunction
-from qpth.qp import QPSolvers
-
 import numpy as np
-from scipy.optimize import minimize
-from scipy.optimize import LinearConstraint
+
+import forwards
 
 import pdb
 
 soft_nonnegativity = False
 soft_lambdas = False
+force_max_dissipation_qp = False
+force_max_dissipation_ext = False
+
+forwards_fn = forwards.get_fowards_function(
+        soft_nonnegativity, soft_lambdas,
+        force_max_dissipation_qp, force_max_dissipation_ext) 
+
+class Parameters:
+    def __init__(self, mu):
+        self.mu = mu
+
+physics_variables = forwards.get_fowards_function(
+        soft_nonnegativity, soft_lambdas,
+        force_max_dissipation_qp, force_max_dissipation_ext) 
 
 class PhysicsNet(torch.nn.Module):
     def __init__(self, startmu):
@@ -22,105 +33,15 @@ class PhysicsNet(torch.nn.Module):
         self.mu = Parameter(torch.tensor([startmu]))
 
     def forward(self, data):
-        n = data.shape[0]
-        vs = torch.unsqueeze(data[:, 0], 1)
-        vnexts = torch.unsqueeze(data[:, 1], 1)
-        us = torch.unsqueeze(data[:, 2], 1)
-
-        mu = self.mu
-
-        beta = vnexts - vs - us
-        
-        pad_base = torch.zeros(6)
-
-        G = torch.tensor([[1.0, -1, 1], [-1, 1, 1], [-1, -1, 0]]).repeat(n, 1, 1)
-        
-        Gpad = torch.tensor([[1.0, -1, 1, 0, 0, 0, 0, 0, 0],
-                             [-1, 1, 1, 0, 0, 0, 0, 0, 0],
-                             [-1, -1, 0, 0, 0, 0, 0, 0, 0],
-                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                             [0, 0, 0, 0, 0, 0, 0, 0, 0]]).repeat(n, 1, 1)
-        
-        fmats = torch.tensor([[1.0, 1, 0], 
-                              [-1, -1, 0],
-                              [0, 0, 1]]).unsqueeze(0).repeat(n, 1, 1)
-        fvecs = torch.cat((vs, us, mu * torch.ones(vs.shape)), 1).unsqueeze(2)
-        f = torch.bmm(fmats, fvecs)
-        batch_zeros = torch.zeros(f.shape)
-        fpad = torch.cat((f, batch_zeros, batch_zeros), 1)
-        
-        # For prediction error
-        A = torch.tensor([[1.0, -1, 0, 0, 0, 0, 0, 0, 0],
-                          [-1, 1, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0]]).repeat(n, 1, 1)
-        beta_zeros = torch.zeros(beta.shape)
-        b = torch.cat((-2 * beta, 2 * beta, beta_zeros, beta_zeros, beta_zeros, 
-                       beta_zeros, beta_zeros, beta_zeros, beta_zeros), 1).unsqueeze(2)
-        
-        inequality_slack_penalty = torch.tensor([0.0, 0, 0, 1, 1, 1, 0, 0, 0]).repeat(n, 1).unsqueeze(2)
-        lambdas_slack_penalty = torch.tensor([0.0, 0, 0, 0, 0, 0, 1, 1, 1]).repeat(n, 1).unsqueeze(2)
-
-        a1 = 1
-        a2 = 1
-        a3 = 100
-        a4 = 100
-
-        Q = 2 * a1 * A + 2 * a2 * Gpad
-        p = a1 * b + a2 * fpad + a3 * inequality_slack_penalty + a4 * lambdas_slack_penalty
-        
-        # Constrain slacks (but not lambdas) to be >= 0
-        R = torch.cat((torch.zeros(6, 3), -torch.eye(6)), 1).repeat(n, 1, 1)
-        h = torch.zeros((1, 6)).repeat(n, 1).unsqueeze(2)
-
-        I = torch.eye(3).repeat(n, 1, 1)
-
-        if soft_nonnegativity:
-            # Constrain G lambda + s(1:3) >= 0
-            R = torch.cat((R, -torch.cat((G, I, torch.zeros(n, 3, 3)), 2)), 1)
-        else:
-            # Constrain G lambda + f >= 0
-            R = torch.cat((R, torch.cat((-G, torch.zeros(n, 3, 6)), 2)), 1)
-
-        # This is the same with soft or hard nonnegativity constraint
-        h = torch.cat((h, f), 1)
-        
-        if soft_lambdas:
-            # Constrain lambda + s(4:7) >= 0
-            R = torch.cat((R, -torch.cat((I, torch.zeros(n, 3, 3), I), 2)), 1)
-            h = torch.cat((h, torch.zeros(n, 3, 1)), 1)
-        else:
-            # Constrain lambda >= 0
-            R = torch.cat((R, torch.cat((-I, torch.zeros(n, 3, 6)), 2)), 1)
-            h = torch.cat((h, torch.zeros(n, 3, 1)), 1)
-
-        Qmod = 0.5 * (Q + Q.transpose(1, 2)) + 0.001 * torch.eye(9).repeat(n, 1, 1)
-        
-        z = QPFunction(check_Q_spd=False)(Qmod, p.squeeze(2), R, h.squeeze(2), torch.tensor([]), torch.tensor([]))
-
-        lcp_slack = torch.bmm(Gpad, z.unsqueeze(2)) + fpad
-
-        costs = 0.5 * torch.bmm(z.unsqueeze(1), torch.bmm(Qmod, z.unsqueeze(2))) \
-                + torch.bmm(p.transpose(1, 2), z.unsqueeze(2)) + a1 * beta**2
-        
-        pdb.set_trace()
-        return sum(costs)
+        cost, _ = forwards_fn(self, data)
+        return cost
 
 # Previous vel, next vel, u
 data = torch.tensor([[1.0, 2.0, 2.0]])
 
 evolutions = []
 #for startmu in np.linspace(0.1, 10, num=30):
-for startmu in [8.0]:
+for startmu in [10.5]:
     net = PhysicsNet(startmu)
 
     loss_func = torch.nn.MSELoss()
